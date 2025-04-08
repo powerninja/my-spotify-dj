999e62b7f811fa48eaf5fa40eca25478ed766531
powerninja <asdyuy23@gmail.com>
2025-04-08 17:45:24 +0900

    ボタン追加

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,19 @@
 import reactLogo from '../assets/react.svg'
 import '../App.css'
 import { useNavigate } from 'react-router-dom'
-
+import { getTopTracks } from '../lib/spotify'
 function Home() {
   const navigate = useNavigate();
+
+  const handleClick = async () => {
+    try {
+      const topTracks = await getTopTracks();
+      console.log(topTracks);
+      // setTracks(topTracks);
+    } catch (err) {
+      console.error("Error fetching tracks:", err);
+    }
+  };
 
   return (
     <>
@@ -14,6 +24,7 @@ function Home() {
     </div>
     <h1>Vite + React</h1>
     <button onClick={() => navigate('/dj')}>DJ Mode</button>
+    <button onClick={handleClick}>Top Tracks を取得</button>
   </>
   )
 }
